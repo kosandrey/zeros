0ad312a840d92ca6b3d6fabf46fe85b8bbe89706
kosandrey <36664836+kosandrey@users.noreply.github.com>
2018-02-28 13:54:39 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function getZerosCount(number) {
+  var amountOf5 = 0;
+  var stepen5 = 5;
+  while(stepen5 <= number)
+  {
+    stepen5 = stepen5 * 5;
+    amountOf5 ++;
+  }
+  var zeroAmount = 0;
+  for(var i = 1; i <= amountOf5; i++)
+  {
+    var thisStepen = Math.pow(5, i);
+    var thisAmount = Math.floor(number/thisStepen);
+    zeroAmount = zeroAmount + thisAmount;
+  }
+  return zeroAmount;
   // your implementation
 }
